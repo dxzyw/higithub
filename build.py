ca19aa174b81aa11c9927f2e23cadf633736888d
unknown <17621764533@163.com>
2025-01-10 10:11:17 +0800

    build.py

--- build.py
+++ build.py
@@ -15,7 +15,7 @@ if __name__ == "__main__":
         readme_file.write("# 开源周刊\n\n> 记录工程师 的不枯燥生活，欢迎订阅，也欢迎 [推荐](https://github.com/dxzyw/higithub/discussions/22) 你的好东西，Fork 自用可见 [开发文档](https://github.com/dxzyw/weekly/blob/main/Deploy.md)，期待你玩得开心~\n\n")
 
         for root, dirs, filenames in os.walk('./src/pages/daily_article'):
-            filenames = sorted(filenames, key=lambda x: float(re.findall(r"(\d+)", x)[0]), reverse=False)
+            filenames = sorted(filenames, key=lambda x: float(re.findall(r"(\d+)", x)[0]), reverse=True)
             print(filenames)
 
         for index, name in enumerate(filenames):
